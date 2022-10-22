07115795a86dbf15b0b69167a2be3d7b8b49e295
Deep Panchal <deeppanchal2410@gmail.com>
2022-10-22 22:10:04 +0530

    Add all error handler exception handler and uncaught promise rejection handler.

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,33 +1,31 @@
 const { ConnectionStates } = require('mongoose');
 const Product = require('../models/productModel');
+const ErrorHandler = require('../utils/errorHandler');
+const catchAsyncError = require('../middleware/catchAsyncError');
 
 // Create Product
-exports.createProduct = async(req, res, next)=>{
+exports.createProduct = catchAsyncError(async(req, res, next)=>{
     const product = await Product.create(req.body);
 
     res.status(201).json({
         success:true,
         product
     });
-}
+});
 
 // Get all Products
-exports.getAllProducts = async (req, res) =>{
+exports.getAllProducts = catchAsyncError(async (req, res) =>{
     const products = await Product.find()
-
     res.status(200).json({
         products
     });
-}
+});
 
 // Update Product
-exports.updateProduct = async (req, res) => {
+exports.updateProduct = catchAsyncError(async (req, res, next) => {
     let product = await Product.findById(req.params.id);
     if (!product){
-        return res.status(500).json({
-            success: false,
-            message: "Product Not Found..."
-        })
+        return next(new ErrorHandler("Product Not Found", 404))
     }
     product = await Product.findByIdAndUpdate(req.params.id, req.body,{
         new:true,
@@ -37,37 +35,31 @@ exports.updateProduct = async (req, res) => {
         success: true,
         product
     })
-}
+});
 
 // Delete Product
-exports.deleteProduct = async (req, res) =>{
-    const product = await Product.findById(req.params.id);
-    if (product){
-        await product.deleteOne();
-        return res.status(200).json({
-            success: true,
-            message: "Successfully Deleted the Product..."
-        })
-    }
-    res.status(500).json({
-        success: false,
-        message: "Product Not Found..."
-    })
-    
-}
-
-// Get single Product by ID
-exports.getSingleProduct = async (req, res) =>{
+exports.deleteProduct = catchAsyncError(async (req, res, next) =>{
     const product = await Product.findById(req.params.id);
     if (!product){
-        return res.status(500).json({
-            success: false,
-            message: "Product Not Found..."
-        })
+        return next(new ErrorHandler("Product Not Found", 404))
+    }
+    await product.remove(); // or we can use product.deleteOne()
+    res.status(200).json({
+        success: true,
+        message: "Successfully Deleted the Product..."
+    })
+    
+});
+
+// Get single Product by ID
+exports.getSingleProduct = catchAsyncError(async (req, res, next) =>{
+    const product = await Product.findById(req.params.id);
+    if (!product){
+        return next(new ErrorHandler("Product Not Found", 404))
     }
     res.status(200).json({
         success:true,
         product
     })
 
-}
+});
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,33 @@
-// const express = require('express');
-
 const app = require('./app');
-
 const dotenv = require('dotenv');
-
 const connectDatabase = require('./config/database');
-// Config Path
 
+// Handling Uncaught Exception Like (console.log(deep) --- where deep is not defined)
+process.on("uncaughtException", (err)=>{
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down the server due to Uncaught Exception`);
+
+    process.exit(1);
+})
+
+// Config Path
 dotenv.config({path: "backend/config/config.env"});
 
 // Connect to Db
 connectDatabase();
 
-app.listen(process.env.PORT, ()=>{
+const server = app.listen(process.env.PORT, ()=>{
     console.log(">>>>>>>>>>> Server is Working >>>>>>>>>>>>")
-})
+});
+
+
+// Unhandled Promises Rejection (If some change occured in config.env file)
+process.on("unhandledRejection", err=>{
+    console.log(`Error: ${err.messsage}`);
+    console.log(`Shutting down the server due to Unhandled Promises Rejection.`);
+
+    server.close(()=>{
+        process.exit(1);
+    });
+});
+
