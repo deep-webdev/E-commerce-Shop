93bc13f3397489cc4f8cb9c799b0a8a3b1ba0e35
Deep Panchal <deeppanchal2410@gmail.com>
2022-10-22 18:05:16 +0530

    Add: create update delete and get operations for product.

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,73 @@
-exports.getAllProducts = (req, res) =>{
-    res.status(200).json({message: "the route is working fine..."});
+const { ConnectionStates } = require('mongoose');
+const Product = require('../models/productModel');
+
+// Create Product
+exports.createProduct = async(req, res, next)=>{
+    const product = await Product.create(req.body);
+
+    res.status(201).json({
+        success:true,
+        product
+    });
+}
+
+// Get all Products
+exports.getAllProducts = async (req, res) =>{
+    const products = await Product.find()
+
+    res.status(200).json({
+        products
+    });
+}
+
+// Update Product
+exports.updateProduct = async (req, res) => {
+    let product = await Product.findById(req.params.id);
+    if (!product){
+        return res.status(500).json({
+            success: false,
+            message: "Product Not Found..."
+        })
+    }
+    product = await Product.findByIdAndUpdate(req.params.id, req.body,{
+        new:true,
+        runValidators: true
+    });
+    res.status(200).json({
+        success: true,
+        product
+    })
+}
+
+// Delete Product
+exports.deleteProduct = async (req, res) =>{
+    const product = await Product.findById(req.params.id);
+    if (product){
+        await product.deleteOne();
+        return res.status(200).json({
+            success: true,
+            message: "Successfully Deleted the Product..."
+        })
+    }
+    res.status(500).json({
+        success: false,
+        message: "Product Not Found..."
+    })
+    
+}
+
+// Get single Product by ID
+exports.getSingleProduct = async (req, res) =>{
+    const product = await Product.findById(req.params.id);
+    if (!product){
+        return res.status(500).json({
+            success: false,
+            message: "Product Not Found..."
+        })
+    }
+    res.status(200).json({
+        success:true,
+        product
+    })
+
 }
---
+++ backend/models/productModel.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose');
+
+const productSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required: [true, "Please Enter Product Name..."],
+        maxLength:[120, "Product Name must be within 120 Character..."]
+    },
+    description:{
+        type:String,
+        required: [true, "Describe your Product First..."]
+    },
+    price:{
+        type:Number,
+        required: [true, "Please Enter product price..."],
+        maxLength: [9, "Price is not Valid for your product..."]
+    },
+    rating:{
+        type:Number,
+        default:0
+    },
+    images:[
+        {
+            public_id:{
+                type:String,
+                required: true
+            },
+            url:{
+                type:String,
+                required: true
+            }
+        }
+    ],
+    category:{
+        type:String,
+        required: [true, "Please Enter Product Category..."]
+    },
+    stock:{
+        type:Number,
+        required: [true, "Please Enter Product Stock..."],
+        maxLength: [4],
+        default: 1
+    },
+    numOfReviews:{
+        type:Number,
+        default:0
+    },
+    reviews: [
+        {
+            name:{
+                type:String,
+                required:true
+            },
+            rating:{
+                type:Number,
+                required:true
+            },
+            comment:{
+                type:String,
+            }
+        }
+    ],
+    createdAt:{
+        type:Date,
+        default: Date.now
+    }
+
+});
+
+module.exports = mongoose.model("Product", productSchema);
